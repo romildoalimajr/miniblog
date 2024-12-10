59acd2c8d605e82dbc39326f903797a0d9f0bb6d
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-12-09 23:13:58 -0300

    Criando a tela de cadastro de usuário.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 
 
@@ -20,6 +22,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
         <Footer />
